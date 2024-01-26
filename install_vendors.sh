16f65878dcbc8dc5504f99fac56e2a80052b0ad2
Carlos A. Cabrera <cacabrer@adobe.com>
2024-01-26 13:23:20 -0600

    renamed scripts to __vendors__

--- install_vendors.sh
+++ install_vendors.sh
@@ -6,17 +6,17 @@ declare -a files_to_copy=(
 )
 
 # Remove existing dropins folder
-rm -rf vendors/
+rm -rf scripts/__vendors__
 
-# Create vendors directory if not exists
-mkdir -p vendors/
+# Create scripts/__vendors__ directory if not exists
+mkdir -p scripts/__vendors__
 
-# Copy specified files from node_modules/@dropins to vendors/dropins
+# Copy specified files from node_modules/@dropins to scripts/__vendors__/dropins
 for file in "${files_to_copy[@]}"; do
-  cp -R "node_modules/$file" "vendors/."
+  cp -R "node_modules/$file" "scripts/__vendors__/."
 done
 
 # Remove package.json files inside dropins
-find vendors -type f -name "package.json" -exec rm {} \;
+find scripts/__vendors__ -type f -name "package.json" -exec rm {} \;
 
 echo "🫡 Vendors installed successfully!"
