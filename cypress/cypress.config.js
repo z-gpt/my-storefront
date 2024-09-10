c7ea043bc2edecbfc4d0275bb3340449f3b6b370
Devagouda Patil <depatil@adobe.com>
2024-09-10 11:25:45 -0500

    update config

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -2,13 +2,13 @@ const { defineConfig } = require('cypress')
 
 
 module.exports = defineConfig({
-  defaultCommandTimeout: 60000,
+  defaultCommandTimeout: 30000,
   screenshotsFolder: 'screenshots',
   downloadsFolder: 'downloads',
   fixturesFolder: 'src/fixtures',
   video: false,
-  pageLoadTimeout: 60000,
-  requestTimeout: 60000,
+  pageLoadTimeout: 30000,
+  requestTimeout: 30000,
   viewportHeight: 900,
   viewportWidth: 1440,
   scrollBehavior: 'nearest',
