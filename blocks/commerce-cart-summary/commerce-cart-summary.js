43d2f6bb14e9eddf3c0d132783ae56c7485f84bc
Anthoula Wojczak <awojczak@adobe.com>
2024-09-23 16:26:38 -0500

    USF-1510: Cart Summary has a Footer Slot
    - fix lint

--- blocks/commerce-cart-summary/commerce-cart-summary.js
+++ blocks/commerce-cart-summary/commerce-cart-summary.js
@@ -1,37 +1,37 @@
-import { render as provider } from "@dropins/storefront-cart/render.js";
-import CartSummaryList from "@dropins/storefront-cart/containers/CartSummaryList.js";
-import { readBlockConfig } from "../../scripts/aem.js";
+import { render as provider } from '@dropins/storefront-cart/render.js';
+import CartSummaryList from '@dropins/storefront-cart/containers/CartSummaryList.js';
+import { readBlockConfig } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
   const {
-    "hide-heading": hideHeading = "false",
-    "start-shopping-url": startShoppingURL = "",
-    "max-items": maxItems,
-    "hide-attributes": hideAttributes = "",
-    "enable-item-quantity-update": enableUpdateItemQuantity = "false",
-    "enable-item-remove": enableRemoveItem = "true",
+    'hide-heading': hideHeading = 'false',
+    'start-shopping-url': startShoppingURL = '',
+    'max-items': maxItems,
+    'hide-attributes': hideAttributes = '',
+    'enable-item-quantity-update': enableUpdateItemQuantity = 'false',
+    'enable-item-remove': enableRemoveItem = 'true',
   } = readBlockConfig(block);
 
-  block.innerHTML = "";
+  block.innerHTML = '';
 
   return provider.render(CartSummaryList, {
-    hideHeading: hideHeading === "true",
+    hideHeading: hideHeading === 'true',
     routeProduct: (product) => `/products/${product.url.urlKey}/${product.sku}`,
     routeEmptyCartCTA: startShoppingURL ? () => startShoppingURL : undefined,
     maxItems: parseInt(maxItems, 10) || undefined,
     attributesToHide: hideAttributes
-      .split(",")
+      .split(',')
       .map((attr) => attr.trim().toLowerCase()),
-    enableUpdateItemQuantity: enableUpdateItemQuantity === "true",
-    enableRemoveItem: enableRemoveItem === "true",
+    enableUpdateItemQuantity: enableUpdateItemQuantity === 'true',
+    enableRemoveItem: enableRemoveItem === 'true',
     slots: {
       Footer: (ctx) => {
         // Prepend Product Promotions
         const discountLabel = ctx.item?.discount?.label;
         if (discountLabel) {
           discountLabel.map((label) => {
-            const discount = document.createElement("div");
-            discount.style.color = "#3d3d3d";
+            const discount = document.createElement('div');
+            discount.style.color = '#3d3d3d';
             discount.innerText = label;
             ctx.appendChild(discount);
           });
