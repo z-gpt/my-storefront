922a48be1a05b42d1d37a88bd77070ccc743b955
Devagouda Patil <depatil@adobe.com>
2024-09-30 21:18:54 -0500

    revert url

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -18,7 +18,7 @@ module.exports = defineConfig({
     runMode: 2,
   },
   e2e: {
-    baseUrl: 'https://test-cypress--aem-boilerplate-commerce--hlxsites.aem.page/',
+    baseUrl: 'http://localhost:3000/',
     supportFile: 'src/support/index.js',
     specPattern: 'src/tests/**/*.spec.js',
   },
