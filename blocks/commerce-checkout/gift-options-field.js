1b8e98553878064d815a8feb9a5ef9cbd624f2ed
jcalcaben <calcaben@adobe.com>
2024-10-24 14:21:48 -0500

    Create gift-options-field custom element

---
+++ blocks/commerce-checkout/gift-options-field.js
@@ -0,0 +1,103 @@
+import { Button, Input, TextArea, provider as UI } from '@dropins/tools/components.js';
+
+const sdkStyle = document.querySelector('style[data-dropin="sdk"]');
+const checkoutStyle = document.querySelector('style[data-dropin="checkout"]');
+
+class GiftOptionsField extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+
+        this._submitGiftMessageHandler = (event) => {
+            event.preventDefault();
+        }
+    }
+
+    set submitGiftMessageHandler(callback) {
+        this._submitGiftMessageHandler = callback;
+    }
+
+    connectedCallback() {
+        this._formTemplate = document.createElement('template');
+
+        this._formTemplate.innerHTML = `
+            <h2 class="checkout-payment-methods__title">Gift Options</h2>
+            <form id="gift-options-form" class="checkout-fields-form__form">
+                <div class="fromName-wrapper"></div>
+                <div class="toName-wrapper"></div>
+                <div class="giftMessage-wrapper dropin-field dropin-field--multiline"></div>
+                <input type="hidden" name="cartId" />
+                <div class="submit-wrapper"></div>
+            </form>
+        `;
+
+
+        this._displayMessageTemplate = document.createElement('template');
+
+        this._displayMessageTemplate.innerHTML = `
+            <p id="gift-message"></p>
+        `;
+        this.render();
+    }
+
+    static get observedAttributes() {
+        return ['cartId', 'giftMessage', 'fromName', 'toName', 'loading'];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.render();
+    }
+
+    render() {
+        this.shadowRoot.innerHTML = '';
+
+        this.shadowRoot.appendChild(this._formTemplate.content.cloneNode(true));
+        this.shadowRoot.querySelector('input[name="cartId"]').value = this.getAttribute('cartId');
+        this.shadowRoot.querySelector('#gift-options-form').addEventListener('submit', this._submitGiftMessageHandler?.bind(this));
+
+        const submitWrapper = this.shadowRoot.querySelector('.submit-wrapper');
+        const fromNameWrapper = this.shadowRoot.querySelector('.fromName-wrapper');
+        const toNameWrapper = this.shadowRoot.querySelector('.toName-wrapper');
+        const giftMessageWrapper = this.shadowRoot.querySelector('.giftMessage-wrapper');
+
+        UI.render(Input, 
+            {
+                type: "text",
+                name: "toName",
+                placeholder: "To name",
+                floatingLabel: "To name",
+                value: this.getAttribute('toName'),
+                disabled: !!this.hasAttribute('loading')
+            })(toNameWrapper);
+        UI.render(Input, 
+            {
+                type: "text",
+                name: "fromName",
+                placeholder: "From name",
+                floatingLabel: "From name",
+                value: this.getAttribute('fromName'),
+                disabled: !!this.hasAttribute('loading')
+            })(fromNameWrapper);
+        UI.render(TextArea, 
+            {
+                name: "giftMessage",
+                placeholder: "Message",
+                value: this.getAttribute('giftMessage'),
+                disabled: !!this.hasAttribute('loading')
+            })(giftMessageWrapper);
+        UI.render(Button, 
+            {
+                variant: "primary",
+                children: "Add Message",
+                type: "submit",
+                enabled: true,
+                size: "medium",
+                disabled: !!this.hasAttribute('loading')
+            })(submitWrapper);
+
+        this.shadowRoot.appendChild(sdkStyle.cloneNode(true));
+        this.shadowRoot.appendChild(checkoutStyle.cloneNode(true));
+    }
+}
+
+customElements.define('gift-options-field', GiftOptionsField);
