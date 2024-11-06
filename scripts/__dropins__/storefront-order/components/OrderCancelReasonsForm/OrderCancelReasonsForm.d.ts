9d2e7c965ce74a1acf58356049271b927f364dda
Abrasimov Yaroslav <y.abrasimov@atwix.com>
2024-11-06 21:46:39 +0100

    Bump storefront-order version

--- scripts/__dropins__/storefront-order/components/OrderCancelReasonsForm/OrderCancelReasonsForm.d.ts
+++
@@ -1,5 +0,0 @@
-import { FunctionComponent } from 'preact';
-import { OrderCancelReasonFormProps } from '../../types';
-
-export declare const OrderCancelReasonsForm: FunctionComponent<OrderCancelReasonFormProps>;
-//# sourceMappingURL=OrderCancelReasonsForm.d.ts.map
