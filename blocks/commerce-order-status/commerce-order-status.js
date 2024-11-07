92ab39a20f257e06f19c9a83f384b08bb9c80f37
Konstantin Fandelyuk <k.fandeliuk@atwix.com>
2024-11-07 23:39:03 +0200

    updated

--- blocks/commerce-order-status/commerce-order-status.js
+++ blocks/commerce-order-status/commerce-order-status.js
@@ -1,14 +1,17 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/no-extraneous-dependencies */
-import { render as orderRenderer } from '@dropins/storefront-order/render.js';
-import { OrderStatus } from '@dropins/storefront-order/containers/OrderStatus.js';
-import { checkIsAuthenticated } from '../../scripts/configs.js';
+import { render as orderRenderer } from "@dropins/storefront-order/render.js";
+import { OrderStatus } from "@dropins/storefront-order/containers/OrderStatus.js";
+import { checkIsAuthenticated } from "../../scripts/configs.js";
 
 // Initialize
-import '../../scripts/initializers/order.js';
+import "../../scripts/initializers/order.js";
 
 export default async function decorate(block) {
   await orderRenderer.render(OrderStatus, {
-    routeCreateReturn: (orderData) => (checkIsAuthenticated() ? `/customer/create-return?orderId=${orderData.id}` : `/create-return?orderId=${orderData.id}`),
+    routeCreateReturn: (orderData) =>
+      checkIsAuthenticated()
+        ? `/customer/create-return?orderRef=${orderData.number}`
+        : `/create-return?orderRef=${orderData.number}`,
   })(block);
 }
