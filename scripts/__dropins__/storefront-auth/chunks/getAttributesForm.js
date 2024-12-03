2e566d65aea0b00cf137b5b518a63afb777e0d04
Konstantin Fandelyuk <k.fandeliuk@atwix.com>
2024-12-03 16:32:19 +0200

    auth updated 2

--- scripts/__dropins__/storefront-auth/chunks/getAttributesForm.js
+++ scripts/__dropins__/storefront-auth/chunks/getAttributesForm.js
@@ -1,6 +1,6 @@
 /*! Copyright 2024 Adobe
 All Rights Reserved. */
-import{f as a,d as o,h as s}from"./getStoreConfig.js";import{t as i}from"./transform-attributes-form.js";const n=`
+import{f as o,h as a}from"./network-error.js";import{t as s}from"./transform-attributes-form.js";import{h as i}from"./getStoreConfig.js";const n=`
   query GET_ATTRIBUTES_FORM($formCode: String!) {
     attributesForm(formCode: $formCode) {
       items {
@@ -32,4 +32,4 @@ import{f as a,d as o,h as s}from"./getStoreConfig.js";import{t as i}from"./trans
       }
     }
   }
-`,m=async t=>await a(n,{method:"GET",cache:"force-cache",variables:{formCode:t}}).then(r=>{var e;return(e=r.errors)!=null&&e.length?o(r.errors):i(r)}).catch(s);export{m as g};
+`,c=async e=>await o(n,{method:"GET",cache:"force-cache",variables:{formCode:e}}).then(r=>{var t;return(t=r.errors)!=null&&t.length?i(r.errors):s(r)}).catch(a);export{c as g};
