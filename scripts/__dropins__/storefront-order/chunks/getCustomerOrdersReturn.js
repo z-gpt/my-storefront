499a5623bf5930b84e97fdf14aa5ce8cb21cee15
Konstantin Fandelyuk <k.fandeliuk@atwix.com>
2024-12-05 20:37:58 +0200

    updated order return paginator

--- scripts/__dropins__/storefront-order/chunks/getCustomerOrdersReturn.js
+++ scripts/__dropins__/storefront-order/chunks/getCustomerOrdersReturn.js
@@ -1,6 +1,6 @@
 /*! Copyright 2024 Adobe
 All Rights Reserved. */
-import{h as R}from"./network-error.js";import{R as E,P as _,a as T,G as s,O as c,t as n}from"./transform-customer-orders-returns.js";import{f as o}from"./fetch-graphql.js";const u=`
+import{h as R}from"./network-error.js";import{R as E,P as _,a as T,G as s,O as n,t as c}from"./transform-customer-orders-returns.js";import{f as o}from"./fetch-graphql.js";const u=`
 query GET_CUSTOMER_ORDERS_RETURN($currentPage: Int, $pageSize: Int) {
  customer {
   returns(currentPage: $currentPage, pageSize: $pageSize) {
@@ -17,5 +17,5 @@ ${E}
 ${_}
 ${T}
 ${s}
-${c}
-`,G=async(t=10,a)=>await o(u,{method:"GET",cache:"force-cache",variables:{pageSize:t,currentPage:a}}).then(r=>{var e;return n((e=r==null?void 0:r.data)==null?void 0:e.customer.returns)}).catch(R);export{G as g};
+${n}
+`,G=async(t=10,a)=>await o(u,{method:"GET",cache:"no-cache",variables:{pageSize:t,currentPage:a}}).then(r=>{var e;return c((e=r==null?void 0:r.data)==null?void 0:e.customer.returns)}).catch(R);export{G as g};
