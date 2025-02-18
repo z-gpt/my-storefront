4a045afc99250bed1f5337e14a4dc59dff889c41
Konstantin Fandelyuk <k.fandeliuk@atwix.com>
2025-02-18 14:46:38 +0200

    updated order dropin and added slot to OrderProductList

--- scripts/__dropins__/storefront-order/components/OrderCostSummaryContent/121.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=121.d.ts.map
--- scripts/__dropins__/storefront-order/components/OrderCostSummaryContent/Blocks/Coupons.d.ts
+++
@@ -1,6 +0,0 @@
-import { OrderDataModel } from '../../../data/models';
-
-export declare const Coupons: ({ order }: {
-    order: OrderDataModel;
-}) => import("preact").JSX.Element;
-//# sourceMappingURL=Coupons.d.ts.map
