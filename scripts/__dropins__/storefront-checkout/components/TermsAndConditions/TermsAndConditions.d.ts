8bb3f2c7a9f6ee5dc0637939de34103d388cf338
Jaume Capdevila <capdevil@adobe.com>
2025-02-26 15:01:41 +0100

    update storefront-chekout dropin

--- scripts/__dropins__/storefront-checkout/components/TermsAndConditions/TermsAndConditions.d.ts
+++ scripts/__dropins__/storefront-checkout/components/TermsAndConditions/TermsAndConditions.d.ts
@@ -2,9 +2,9 @@ import { FunctionComponent, VNode } from 'preact';
 import { HTMLAttributes } from 'preact/compat';
 
 export interface TermsAndConditionsProps extends HTMLAttributes<HTMLFormElement> {
-    isInitialized?: boolean;
     agreements?: VNode;
     error?: string;
+    isInitialized?: boolean;
 }
 export declare const TermsAndConditions: FunctionComponent<TermsAndConditionsProps>;
 //# sourceMappingURL=TermsAndConditions.d.ts.map
