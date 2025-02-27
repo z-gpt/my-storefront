c26afd310e32b516763b86e1b6b4b573ba828054
Konstantin Fandelyuk <k.fandeliuk@atwix.com>
2025-02-27 13:18:40 +0200

    added order dropin to account

--- blocks/commerce-order-product-list/commerce-order-product-list.js
+++ blocks/commerce-order-product-list/commerce-order-product-list.js
@@ -2,6 +2,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { render as orderRenderer } from '@dropins/storefront-order/render.js';
 import { OrderProductList } from '@dropins/storefront-order/containers/OrderProductList.js';
+import GiftOptions from '@dropins/storefront-cart/containers/GiftOptions.js';
+import { render as CartProvider } from '@dropins/storefront-cart/render.js';
 
 // Initialize
 import '../../scripts/initializers/order.js';
@@ -9,5 +11,19 @@ import '../../scripts/initializers/order.js';
 export default async function decorate(block) {
   await orderRenderer.render(OrderProductList, {
     routeProductDetails: (product) => `/products/${product.productUrlKey}/${product.product.sku}`,
+    slots: {
+      Footer: (ctx) => {
+        const giftOptions = document.createElement('div');
+
+        CartProvider.render(GiftOptions, {
+          item: ctx.item,
+          view: 'product',
+          dataSource: 'order',
+          isEditable: false,
+        })(giftOptions);
+
+        ctx.appendChild(giftOptions);
+      },
+    },
   })(block);
 }
