d96659458b1116c68e32730b75b7058f921e5be7
Marc Ginesta <mginesta@adobe.com>
2025-04-22 18:03:20 +0200

    Import wishlist dropin and initialize it

---
+++ scripts/initializers/wishlist.js
@@ -0,0 +1,18 @@
+import { initializers } from '@dropins/tools/initializer.js';
+import { initialize, setFetchGraphQlHeaders } from '@dropins/storefront-wishlist/api.js';
+import { initializeDropin } from './index.js';
+import { fetchPlaceholders } from '../commerce.js';
+import { getHeaders } from '../configs.js';
+
+await initializeDropin(async () => {
+  setFetchGraphQlHeaders(getHeaders('checkout'));
+
+  const labels = await fetchPlaceholders();
+  const langDefinitions = {
+    default: {
+      ...labels,
+    },
+  };
+
+  return initializers.mountImmediately(initialize, { langDefinitions });
+})();
