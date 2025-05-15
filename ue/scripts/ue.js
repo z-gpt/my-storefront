f136f729818b7f69e6fef14dc76d72187add991f
Markus Haack <mhaack@adobe.com>
2025-05-15 11:49:40 +0200

    fix: ue scripts

--- ue/scripts/ue.js
+++ ue/scripts/ue.js
@@ -10,7 +10,6 @@
  * governing permissions and limitations under the License.
  */
 
-import { showSlide } from '../../blocks/carousel/carousel.js';
 import { moveInstrumentation } from './ue-utils.js';
 
 const setupObservers = () => {
@@ -51,28 +50,6 @@ const setupObservers = () => {
               }
             }
             break;
-          case 'accordion':
-            if (addedElements.length === 1 && addedElements[0].tagName === 'DETAILS') {
-              moveInstrumentation(removedElements[0], addedElements[0]);
-              moveInstrumentation(removedElements[0].querySelector('div'), addedElements[0].querySelector('summary'));
-            }
-            break;
-          case 'carousel':
-            if (removedElements.length === 1 && removedElements[0].attributes['data-aue-model']?.value === 'carousel-item') {
-              const resourceAttr = removedElements[0].getAttribute('data-aue-resource');
-              if (resourceAttr) {
-                const itemMatch = resourceAttr.match(/item-(\d+)/);
-                if (itemMatch && itemMatch[1]) {
-                  const slideIndex = parseInt(itemMatch[1], 10);
-                  const slides = mutation.target.querySelectorAll('li.carousel-slide');
-                  const targetSlide = Array.from(slides).find((slide) => parseInt(slide.getAttribute('data-slide-index'), 10) === slideIndex);
-                  if (targetSlide) {
-                    moveInstrumentation(removedElements[0], targetSlide);
-                  }
-                }
-              }
-            }
-            break;
           default:
             break;
         }
@@ -85,55 +62,6 @@ const setupObservers = () => {
   });
 };
 
-const setupUEEventHandlers = () => {
-  document.addEventListener('aue:ui-select', (event) => {
-    const { detail } = event;
-    const resource = detail?.resource;
-
-    if (resource) {
-      const element = document.querySelector(`[data-aue-resource="${resource}"]`);
-      if (!element) {
-        return;
-      }
-      const blockEl = element.parentElement?.closest('.block[data-aue-resource]') || element?.closest('.block[data-aue-resource]');
-      if (blockEl) {
-        const block = blockEl.getAttribute('data-aue-model');
-        const index = element.getAttribute('data-slide-index');
-
-        switch (block) {
-          case 'accordion':
-            blockEl.querySelectorAll('details').forEach((details) => {
-              details.open = false;
-            });
-            element.open = true;
-            break;
-          case 'carousel':
-            if (index) {
-              showSlide(blockEl, index);
-            }
-            break;
-          case 'tabs':
-            if (element === block) {
-              return;
-            }
-            blockEl.querySelectorAll('[role=tabpanel]').forEach((panel) => {
-              panel.setAttribute('aria-hidden', true);
-            });
-            element.setAttribute('aria-hidden', false);
-            blockEl.querySelector('.tabs-list').querySelectorAll('button').forEach((btn) => {
-              btn.setAttribute('aria-selected', false);
-            });
-            blockEl.querySelector(`[aria-controls=${element?.id}]`).setAttribute('aria-selected', true);
-            break;
-          default:
-            break;
-        }
-      }
-    }
-  });
-};
-
 export default () => {
   setupObservers();
-  setupUEEventHandlers();
 };
