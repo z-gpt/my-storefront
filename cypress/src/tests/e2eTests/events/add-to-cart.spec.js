52f6bbda34c5a521e55750af7fbfcde838fcb2d7
yutingj <yutingj@adobe.com>
2025-05-28 15:34:21 -0500

    add cypress for open-cart

--- cypress/src/tests/e2eTests/events/add-to-cart.spec.js
+++ cypress/src/tests/e2eTests/events/add-to-cart.spec.js
@@ -31,3 +31,27 @@ it("is sent on add to cart button click", () => {
       });
   });
 });
+
+it("open-cart includes changedProductsContext with items", () => {
+  cy.visit(products.configurable.urlPathWithOptions);
+  
+  cy.get(".product-details__buttons__add-to-cart button")
+    .should("be.visible")
+    .click();
+  
+  cy.get('button[data-count="1"]').should("be.visible").click();
+
+  cy.waitForResource("commerce-events-collector.js").then(() => {
+    cy.window()
+      .its("adobeDataLayer")
+      .then((adobeDataLayer) => {
+        const openCartEvent = adobeDataLayer.find(
+          (event) => event?.event === "open-cart"
+        );
+        expect(openCartEvent).to.exist;
+        
+        expect(openCartEvent.changedProductsContext).to.exist;
+        expect(openCartEvent.changedProductsContext.items.length).to.be.greaterThan(0);
+      });
+  });
+});
