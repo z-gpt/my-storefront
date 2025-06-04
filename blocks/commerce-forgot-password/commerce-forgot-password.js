b0b8f5fdda25b7577243333c55538f4df967224a
Jaume Capdevila <capdevil@adobe.com>
2025-06-04 18:02:01 +0200

    display confirmation modal

--- blocks/commerce-forgot-password/commerce-forgot-password.js
+++ blocks/commerce-forgot-password/commerce-forgot-password.js
@@ -10,8 +10,29 @@ import {
 } from "../../scripts/constants.js";
 import { rootLink } from "../../scripts/scripts.js";
 
+// Block-level
+import createModal from "../modal/modal.js";
+
 // Initialize
 import "../../scripts/initializers/auth.js";
+
+let modal = null;
+
+const showConfirmationModal = async () => {
+  const content = document.createElement("div");
+
+  const title = document.createElement("h2");
+  title.textContent = "Check your email";
+
+  const message = document.createElement("p");
+  message.textContent =
+    "If there is an account associated with this email, you will receive a link to reset your password.";
+
+  content.append(title, message);
+
+  modal = await createModal([content]);
+  modal.showModal();
+};
 
 export default async function decorate(block) {
   if (checkIsAuthenticated()) {
@@ -19,8 +40,8 @@ export default async function decorate(block) {
   } else {
     await authRenderer.render(ResetPassword, {
       routeSignIn: () => rootLink(CUSTOMER_LOGIN_PATH),
-      onSuccessCallback: (error) => {
-        console.log("success :>> ", error);
+      onSuccessCallback: () => {
+        showConfirmationModal();
       },
       onErrorCallback: (error) => {
         console.log("error :>> ", error);
