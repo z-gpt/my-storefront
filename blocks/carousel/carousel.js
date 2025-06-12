161666833467f86fdf571db80d743a2ddea3e8cd
Stephen Rugh <rugh@adobe.com>
2025-06-12 14:10:48 -0500

    fix import
    Signed-off-by: Stephen Rugh <rugh@adobe.com>

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,4 +1,4 @@
-import { fetchPlaceholders } from '../../scripts/aem.js';
+import { fetchPlaceholders } from '../../scripts/commerce.js';
 
 function updateActiveSlide(slide) {
   const block = slide.closest('.carousel');
